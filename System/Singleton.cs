358420eb56cddd18c8e2f82d6ea32a416415598c
Shann Stephen Enciso <77877534+GetJynxDEV@users.noreply.github.com>
2025-05-18 18:20:30 -0700

    Create Singleton.cs

---
+++ System/Singleton.cs
@@ -0,0 +1,17 @@
+public class Singleton : MonoBehaviour
+{
+    public static Singleton Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
