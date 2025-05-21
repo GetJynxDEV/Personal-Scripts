0e9e288d1ded64366a524d67274cd6f0c003f880
Shann Stephen Enciso <77877534+GetJynxDEV@users.noreply.github.com>
2025-05-21 09:19:05 -0700

    Update SpawnObjects.cs
    Updated

--- Game Objects/SpawnObjects.cs
+++ Game Objects/SpawnObjects.cs
@@ -2,27 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Instatiate : MonoBehaviour
-{
-    public GameObject projectile;
-    public Transform bulletPos;
+public class Instantiate : MonoBehaviour
+{    
+    //Attach the GameObject Prefab that you want to Instantiate
+    public GameObject prefab;
+    //Attach the Transform of where you want it to spawn
+    public Transform prefabPos;
 
-    public float bulletTimer;
+    //Countdown Timer for when it will spawn
+    public float elapsedTimer;
+    //Set Max Timer for Countdown Timer
+    public float maxTimer = 2f;
 
     private void Update()
     { 
-        bulletTimer = Time.deltaTime;
+        //Elapsed Timer will now start counting
+        elapsedTimer = Time.deltaTime;
 
-        if (bulletTimer > phaseTime)
+        //If Elapsed Timer is greater than Max Timer Function will be called
+        if (elapsedTimer > maxTimer)
         {
-            bulletTimer = 0;
+            //Elapsed Timer will be set to 0 again
+            elapsedTimer = 0;
 
-            shootProjectile();
+            //Function to Instantiate Game Object
+            spawnPrefab();
         }
     }
 
-    private void shootProjectile()
+    private void spawnPrefab()
     {
-        Instantiate(projectile, bulletPos.position, Quaternion.identity);
+        //This is how you will Instantiate a Game Object
+        //Call the Object you want to Instantiate, then the Position you want, then last is the Rotation
+        Instantiate(prefab, prefabPos.position, Quaternion.identity);
     }
 }
